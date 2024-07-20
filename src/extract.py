b18e8fa74198a3d80590299564a7e53d09ff3f7a
Suso <jesusmosteiro@gmail.com>
2024-07-20 21:39:29 +0200

    Structure handling

--- src/extract.py
+++ src/extract.py
@@ -2,6 +2,7 @@ from extractor_pass import ExtractorPass
 from dictionary import Dictionary
 from world import World
 
+import itertools as it
 from glob import glob
 from tqdm import tqdm
 import json
@@ -9,10 +10,11 @@ import os
 
 from amulet.api.data_types import VersionIdentifierType, Dimension, ChunkCoordinates
 from amulet.api.chunk.entity_list import EntityList
+from amulet.api.level.world import World as Level
 from amulet.api.block_entity import BlockEntity
-from amulet.api.level.world import World
 from amulet.api.entity import Entity
 from amulet.api.chunk import Chunk
+import amulet_nbt as nbt
 
 from typing import TYPE_CHECKING
 if TYPE_CHECKING: from settings import Settings # Why must python torture me like this
@@ -32,17 +34,14 @@ def handle_tile(tile: BlockEntity, dictionary: Dictionary, extractors: list) -> 
     return sum(extractor.extract(dictionary, tile) for extractor in extractors if tile.base_name in extractor.match_tiles)
 
 def handle_chunk(chunk: Chunk, dictionary: Dictionary, extractors: list) -> None:
-    for block_entity in chunk.block_entities:
-        chunk.changed |= bool(handle_tile(block_entity, dictionary, extractors))
+    chunk.changed = not all(not bool(handle_tile(block_entity, dictionary, extractors)) for block_entity in chunk.block_entities)
 
 def handle_entity(entity: Entity, dictionary: Dictionary, extractors: list) -> int:
     return sum(extractor.extract(dictionary, entity) for extractor in extractors if entity.base_name in extractor.match_entities)
 
-def handle_entities(entities: tuple[EntityList,VersionIdentifierType], level: World, coord: ChunkCoordinates, dimension: Dimension, dictionary: Dictionary, extractors: list) -> None:
+def handle_entities(entities: tuple[EntityList,VersionIdentifierType], level: Level, coord: ChunkCoordinates, dimension: Dimension, dictionary: Dictionary, extractors: list) -> None:
     entities = entities[0]
-    changed = False
-    for e in entities:
-        changed |= bool(handle_entity(e, dictionary, extractors))
+    changed = not all(not bool(handle_entity(entity, dictionary, extractors)) for entity in entities)
     if changed:
         level.set_native_entites(*coord, dimension, entities)
 
@@ -68,11 +67,34 @@ def handle_chunks(world: World, settings: 'Settings', dictionary: Dictionary, ex
         world.level.unload()
     world.level.close()
 
+def handle_structures(path: str, dictionary: Dictionary, extractors: dict[ExtractorPass,list]) -> None:
+    for f in it.chain(
+        glob(path + '/generated/*/structures/**/*.nbt', recursive=True),
+        glob(path + '/datapacks/*/data/*/structures/**/*.nbt', recursive=True)
+    ):
+        structure = nbt.load(f)
+        if 'blocks' in structure.tag:
+            for block_tag in filter(lambda x: 'nbt' in x, structure.tag['blocks']):
+                namespace, base_name = str(block_tag['nbt']['id']).split(':')
+                x, y, z = (int(p) for p in block_tag['pos'])
+                tile = BlockEntity(namespace, base_name, x, y, z, block_tag['nbt'])
+                handle_tile(tile, dictionary, extractors[ExtractorPass.TILE])
+
+        if 'entities' in structure.tag:
+            for entity_tag in filter(lambda x: 'nbt' in x, structure.tag['entities']):
+                namespace, base_name = str(entity_tag['nbt']['id']).split(':')
+                x, y, z = (float(p) for p in entity_tag['pos'])
+                entity = Entity(namespace, base_name, x, y, z, entity_tag['nbt'])
+                handle_entity(entity, dictionary, extractors[ExtractorPass.ENTITY])              
+
+        structure.save_to(f)
+
 def extract(world: World, settings: 'Settings') -> None:
     dictionary = Dictionary(settings)
     extractors = {k: [x(settings) for x in settings.extractors[k]] for k in settings.extractors}
 
     handle_chunks(world, settings, dictionary, extractors)
+    handle_structures(world.path, dictionary, extractors)
 
     print(_('Outputting lang to \'{}\'...').format(settings.out_lang))
     lang = dictionary.reverse()
