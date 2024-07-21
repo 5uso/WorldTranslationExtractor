fe7292318f3197f343cdcffd6aceba368929b196
Suso <jesusmosteiro@gmail.com>
2024-07-21 12:58:32 +0200

    Duplicate key handling

--- src/dictionary.py
+++ src/dictionary.py
@@ -5,15 +5,18 @@ class Dictionary():
     def __init__(self, settings: "Settings") -> None:
         self.keepdup = settings.keepdup
         self.data = dict()
+        self.keys = dict()
 
     def add_entry(self, text: str, key: str) -> str:
         if text in self.data:
             if not self.keepdup:
                 return self.data[text]
             
+            key = self.increment_key(key)
             self.data[text].append(key)
             return key
         
+        key = self.increment_key(key)
         self.data[text] = [key] if self.keepdup else key
         return key
     
@@ -28,3 +31,10 @@ class Dictionary():
                 r[k] = text
 
         return r
+    
+    def increment_key(self, key: str) -> str:
+        if key in self.keys:
+            self.keys[key] += 1
+            return key + f'.{self.keys[key]}'
+        self.keys[key] = 0
+        return key
