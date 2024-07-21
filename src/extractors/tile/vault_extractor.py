c689dd7200790f2ccf7c72331af1c17b822babea
Suso <jesusmosteiro@gmail.com>
2024-07-21 16:20:11 +0200

    Vault extractor

---
+++ src/extractors/tile/vault_extractor.py
@@ -0,0 +1,30 @@
+from extractors.tile_extractor import TileExtractor
+from extractor_pass import ExtractorPass
+from dictionary import Dictionary
+from extract import handle_item
+from settings import Settings
+
+from amulet.api.block_entity import BlockEntity
+
+class VaultExtractor(TileExtractor):
+    extractor_name = 'vault'
+    match_tiles = ('vault')
+
+    def __init__(self, settings: Settings) -> None:
+        self.item_extractors = [x(settings) for x in settings.extractors[ExtractorPass.ITEM]]
+
+    def extract(self, dictionary: Dictionary, tile: BlockEntity) -> int:
+        count = 0
+
+        count += handle_item(tile.nbt['config']['key_item'], dictionary, self.item_extractors)
+
+        if 'display_item' in tile.nbt['shared_data']:
+            count += handle_item(tile.nbt['shared_data']['display_item'], dictionary, self.item_extractors)
+
+        if 'items_to_eject' in tile.nbt['server_data']:
+            for item in tile.nbt['server_data']['items_to_eject']:
+                count += handle_item(item, dictionary, self.item_extractors)
+        
+        return count
+
+extractor = VaultExtractor
