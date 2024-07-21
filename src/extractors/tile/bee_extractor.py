85c0a5d8fcfbe19d54449ef69bcd64e8f2fb475a
Suso <jesusmosteiro@gmail.com>
2024-07-21 15:12:53 +0200

    Bee extractor

---
+++ src/extractors/tile/bee_extractor.py
@@ -0,0 +1,29 @@
+from extractors.tile_extractor import TileExtractor
+from extractor_pass import ExtractorPass
+from dictionary import Dictionary
+from extract import handle_entity
+from settings import Settings
+
+from amulet.api.block_entity import BlockEntity
+from amulet.api.entity import Entity
+
+class BeeExtractor(TileExtractor):
+    extractor_name = 'bee'
+    match_tiles = ('beehive', 'bee_nest')
+
+    def __init__(self, settings: Settings) -> None:
+        self.entity_extractors = [x(settings) for x in settings.extractors[ExtractorPass.ENTITY]]
+
+    def extract(self, dictionary: Dictionary, tile: BlockEntity) -> int:
+        count = 0
+
+        for bee in tile.nbt['bees']:
+            namespace, base_name = str(bee['entity_data']['id']).split(':')
+            entity = Entity(namespace, base_name, 0, 0, 0, bee['entity_data'])
+            count += handle_entity(entity, dictionary, self.entity_extractors)
+
+        if count:
+            self.indexes[tile.base_name] += 1
+        return count
+
+extractor = BeeExtractor
