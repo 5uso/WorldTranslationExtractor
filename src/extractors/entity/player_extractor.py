92a6e91a1cdc9eae2d8f6f4d458863ee19b73332
Suso <jesusmosteiro@gmail.com>
2024-07-22 21:22:34 +0200

    Player extractor

---
+++ src/extractors/entity/player_extractor.py
@@ -0,0 +1,32 @@
+from extractors.entity_extractor import EntityExtractor
+from extract import handle_entity, handle_item
+from extractor_pass import ExtractorPass
+from dictionary import Dictionary
+from settings import Settings
+
+from amulet.api.entity import Entity
+
+class PlayerExtractor(EntityExtractor):
+    extractor_name = 'player'
+    match_entities = ('player',)
+
+    def __init__(self, settings: Settings) -> None:
+        self.entity_extractors = [x(settings) for x in settings.extractors[ExtractorPass.ENTITY]]
+        self.item_extractors = [x(settings) for x in settings.extractors[ExtractorPass.ITEM]]
+
+    def extract(self, dictionary: Dictionary, entity: Entity) -> int:
+        count = 0
+
+        if 'EnderItems' in entity.nbt:
+            for item in entity.nbt['EnderItems']:
+                count += handle_item(item, dictionary, self.item_extractors)
+
+        for entity_slot in ('ShoulderEntityLeft', 'ShoulderEntityRight'):
+            if entity_slot in entity.nbt:
+                namespace, base_name = str(entity.nbt[entity_slot]['id']).split(':')
+                entity = Entity(namespace, base_name, 0.0, 0.0, 0.0, entity.nbt[entity_slot])
+                count += handle_entity(entity, dictionary, self.entity_extractors)
+
+        return count
+
+extactor = PlayerExtractor
