70fe82c1b515bc0495576a2cc1f408797ad4a861
Suso <jesusmosteiro@gmail.com>
2024-07-22 20:57:25 +0200

    Container entity extractor

---
+++ src/extractors/entity/container_entity_extractor.py
@@ -0,0 +1,40 @@
+from extractors.entity_extractor import EntityExtractor
+from extractor_pass import ExtractorPass
+from dictionary import Dictionary
+from extract import handle_item
+from settings import Settings
+
+from amulet.api.entity import Entity
+
+class ContainerEntityExtractor(EntityExtractor):
+    extractor_name = 'container_entity'
+    match_entities = {
+        'hopper_minecart': 'Items',
+        'chest_minecart': 'Items',
+        'chest_boat': 'Items',
+        'trader_llama': 'Items',
+        'mule': 'Items',
+        'llama': 'Items',
+        'donkey': 'Items',
+        'wandering_trader': 'Inventory',
+        'villager': 'Inventory',
+        'pillager': 'Inventory',
+        'player': 'Inventory',
+        'piglin': 'Inventory',
+        'allay': 'Inventory'
+    }
+
+    def __init__(self, settings: Settings) -> None:
+        self.item_extractors = [x(settings) for x in settings.extractors[ExtractorPass.ITEM]]
+
+    def extract(self, dictionary: Dictionary, entity: Entity) -> int:
+        count = 0
+
+        tag_name = self.match_entities[entity.base_name]
+        if tag_name in entity.nbt:
+            for item in entity.nbt[tag_name]:
+                count += handle_item(item, dictionary, self.item_extractors)
+        
+        return count
+
+extactor = ContainerEntityExtractor
