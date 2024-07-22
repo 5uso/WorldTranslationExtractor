80f615b2cdf7a814b179a6c0df0597bee6a6b103
Suso <jesusmosteiro@gmail.com>
2024-07-23 00:46:32 +0200

    Function extractor

---
+++ src/extractors/text_file/function_extractor.py
@@ -0,0 +1,19 @@
+from extractors.text_file_extractor import TextFileExtractor
+from dictionary import Dictionary
+
+class FunctionExtractor(TextFileExtractor):
+    extractor_name = 'function'
+    match_filenames = ('.*\.mcfunction',)
+
+    def extract(self, dictionary: Dictionary, file: tuple[list[str],list[str]]) -> int:
+        count = 0
+        path, contents = file
+        key_path = ".".join(path)
+
+        for cmd in range(len(contents)):
+            contents[cmd], n = dictionary.replace_command(contents[cmd], f'function.{key_path}.line{cmd}')
+            count += n
+
+        return count
+
+extractor = FunctionExtractor
