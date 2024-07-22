e53dd77bee1ea945c82e91e37a2558137f390d3c
Suso <jesusmosteiro@gmail.com>
2024-07-22 21:17:06 +0200

    Text display extractor

---
+++ src/extractors/entity/text_display_extractor.py
@@ -0,0 +1,25 @@
+from extractors.entity_extractor import EntityExtractor
+from dictionary import Dictionary
+from settings import Settings
+
+from amulet.api.entity import Entity
+
+class TextDisplayExtractor(EntityExtractor):
+    extractor_name = 'text_display'
+    match_entities = ('text_display',)
+
+    def __init__(self, settings: Settings) -> None:
+        self.index = 1
+
+    def extract(self, dictionary: Dictionary, entity: Entity) -> int:
+        count = 0
+
+        if 'text' in entity.nbt:
+            entity.nbt['text'], n = dictionary.replace_component(entity.nbt['text'], f'text_display.{self.index}.text')
+            count += n
+
+        if count:
+            self.index += 1
+        return count
+
+extactor = TextDisplayExtractor
