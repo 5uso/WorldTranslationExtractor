fde5770eb5dc9e0289ba3ba48ff8b38de38c9590
Suso <jesusmosteiro@gmail.com>
2024-07-22 21:39:02 +0200

    Villager extractor

---
+++ src/extractors/entity/villager_extractor.py
@@ -0,0 +1,27 @@
+from extractors.entity_extractor import EntityExtractor
+from extractor_pass import ExtractorPass
+from dictionary import Dictionary
+from extract import handle_item
+from settings import Settings
+
+from amulet.api.entity import Entity
+
+class VillagerExtractor(EntityExtractor):
+    extractor_name = 'villager'
+    match_entities = ('villager', 'zombie_villager', 'wandering_trader')
+
+    def __init__(self, settings: Settings) -> None:
+        self.item_extractors = [x(settings) for x in settings.extractors[ExtractorPass.ITEM]]
+
+    def extract(self, dictionary: Dictionary, entity: Entity) -> int:
+        count = 0
+
+        if 'Offers' in entity.nbt:
+            for offer in entity.nbt['Offers']['Recipes']:
+                count += handle_item(offer['buy'], dictionary, self.item_extractors)
+                count += handle_item(offer['buyB'], dictionary, self.item_extractors)
+                count += handle_item(offer['sell'], dictionary, self.item_extractors)
+
+        return count
+
+extactor = VillagerExtractor
