427b9aba36384bd21a646c79f090fe3be8c3d12d
Suso <jesusmosteiro@gmail.com>
2024-07-23 02:13:44 +0200

    Matching structure

--- src/dictionary.py
+++ src/dictionary.py
@@ -1,5 +1,7 @@
 from amulet_nbt import StringTag
 
+import re
+
 from typing import TYPE_CHECKING
 if TYPE_CHECKING: from settings import Settings
 
@@ -8,6 +10,7 @@ class Dictionary():
         self.keepdup = settings.keepdup
         self.data = dict()
         self.keys = dict()
+        self.current_matches = 0
 
     def add_entry(self, text: str, key: str) -> str:
         if text in self.data:
@@ -41,11 +44,33 @@ class Dictionary():
         self.keys[key] = 0
         return key
     
+    component_patterns = [
+
+    ]
     def replace_component(self, nbt: StringTag, key: str) -> tuple[StringTag,int]:
-        return nbt, 0
+        text = str(nbt)
+        pattern: re.Pattern
+        self.current_matches = 0
+        for pattern, matcher in self.component_patterns:
+            text = pattern.sub(string=text, repl=matcher(key))
+        return StringTag(text), self.current_matches
     
+    command_patterns = [
+        
+    ]
     def replace_command(self, cmd: str, key: str) -> tuple[str,int]:
-        return cmd, 0
+        pattern: re.Pattern
+        self.current_matches = 0
+        for pattern, matcher in self.command_patterns:
+            cmd = pattern.sub(string=cmd, repl=matcher(key))
+        return cmd, self.current_matches
     
+    other_patterns = [
+        
+    ]
     def replace_other(self, string: str, key: str) -> tuple[str,int]:
-        return string, 0
+        pattern: re.Pattern
+        self.current_matches = 0
+        for pattern, matcher in self.other_patterns:
+            string = pattern.sub(string=string, repl=matcher(key))
+        return string, self.current_matches
