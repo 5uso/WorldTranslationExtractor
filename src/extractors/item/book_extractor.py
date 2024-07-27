e8b4938c4859d0808a9910dbed0e34e617ad5a96
Suso <jesusmosteiro@gmail.com>
2024-07-27 03:19:27 +0200

    Why is the chat filter part of the book nbt format
    in which universe does this design make any sense

--- src/extractors/item/book_extractor.py
+++ src/extractors/item/book_extractor.py
@@ -20,11 +20,11 @@ class BookExtractor(ItemExtractor):
 
         if 'pages' in item['components']['minecraft:written_book_content']:
             for page in range(len(item['components']['minecraft:written_book_content']['pages'])):
-                item['components']['minecraft:written_book_content']['pages'][page], n = dictionary.replace_component(item['components']['minecraft:written_book_content']['pages'][page], f'book.{self.index}.content.page{page}')
+                item['components']['minecraft:written_book_content']['pages'][page]['raw'], n = dictionary.replace_component(item['components']['minecraft:written_book_content']['pages'][page]['raw'], f'book.{self.index}.content.page{page}')
                 count += n
 
         if 'title' in item['components']['minecraft:written_book_content'] and 'minecraft:custom_name' not in item['components']:
-            key = dictionary.add_entry(str(item['components']['minecraft:written_book_content']['title']), f'book.{self.index}.title')
+            key = dictionary.add_entry(str(item['components']['minecraft:written_book_content']['title']['raw']), f'book.{self.index}.title')
             item['components']['minecraft:custom_name'] = StringTag(f'{{"translate":"{key}","italic":false}}')
             count += 1
 
