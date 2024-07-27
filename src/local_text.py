e547a17d07407f8ad74746cfc377990b091b4da3
Suso <jesusmosteiro@gmail.com>
2024-07-28 00:50:53 +0200

    Translation support
    Tries to determine OS language from either LC_MESSAGES env or windows jank

--- src/local_text.py
+++ src/local_text.py
@@ -1,3 +1,13 @@
 import gettext
+import locale
+import ctypes
+import os
 
-gettext.install('world_translation_extractor', localedir='./language')
+preferred_language_code = 'en'
+
+if os.name == 'nt':
+    preferred_language_code = locale.windows_locale[ctypes.windll.kernel32.GetUserDefaultUILanguage()]
+elif (lc_msgs := os.getenv('LC_MESSAGES')):
+    preferred_language_code = lc_msgs
+
+gettext.translation('base', localedir='language', languages=[preferred_language_code,'en']).install()
